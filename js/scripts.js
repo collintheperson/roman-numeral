87ad336ffe323002231758975653a1285b96b19e
collin person and kimberly lu <collin+kimberly@>
2017-07-13 10:18:52 -0700

    add base number converted to base roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-$(function()  {
+$(function() {
+  var numbers = [1,5,10,50,100,500,1000];
+  var romans = ["I", "V", "X", "L", "C", "D", "M"];
+  $("#roman-numerals").submit(function(event) {
+  var input = parseInt($("#number").val());
+  
+  for (var i = 0; i < numbers.length; i++) {
+    if (input === numbers[i]) {
+      numbers[i] = romans[i];
+      alert(romans[i]);
+  }
+  }
 
-
+  event.preventDefault();
+  });
 });
--- readme.md
+++ readme.md
@@ -1,7 +1,10 @@
 # Kimberly Lu's & Collin Person's Great Roman Numeral Project
 
 ### Specifications
-  */ User input single digit 1-3
+  */ User input base number = base roman numerals
+    Input Example: 5
+    Output Example: V
+  User input single digit 1-3
     Input Example: 1
     Output Example: I
   */ User input is 5
